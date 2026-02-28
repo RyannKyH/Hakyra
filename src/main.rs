c037dd8a016e3c886d4b9ada19e32df4d61824b7
Ryann <ryannkyh@gmail.com>
2026-02-28 11:27:40 -0300

    Jogo da Advinhação
    Jogo Bem simples De Acertar o Numero

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rand::RngExt;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let numero = rand::rng().random_range(1..=10);
+    println!("Adivinhe o Numero de 1-10");
+    println!("Qual é o seu Palpite: ");
+    let mut palpite = String::new();
+    io::stdin()
+        .read_line(&mut palpite)
+        .expect("Não foi possivel ler o seu Palpite");
+    let num: i32 = palpite.trim().parse().expect("Error");
+    println!("O seu Palpite foi {palpite} \n e o numero era {numero}");
+    if num == numero {
+        println!("Voce acertou o numero :p")
+    } else {
+        println!("Voce errou o numero :/")
+    }
 }
